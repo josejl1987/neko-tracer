45d84107a74870816039e5a45bed5db846b1237e
esperknyte@gmail.com <esperknyte@gmail.com@cf3c034b-4f64-66e7-851e-5312481bef58>
2011-05-22 21:14:17 +0000

    Initial code commit

---
+++ trunk/np2ver.h
@@ -0,0 +1,10 @@
+
+#define	NP2VER_CORE			"0.83mod8VC6"
+
+// #define	NP2VER_WIN9X
+// #define	NP2VER_MACOS9
+// #define	NP2VER_MACOSX
+// #define	NP2VER_X11
+// #define	NP2VER_WINCE
+// #define	NP2VER_SDLZAU
+
